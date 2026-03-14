a061d1349c3198d4fed0838ab46b90279ef96730
saimondanya <backfire314@gmail.com>
2026-03-14 14:44:42 +0300

    update

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -9,6 +9,17 @@ return {
     },
   },
   keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find files" },
+    {
+      "<leader>fi",
+      function()
+        require("telescope.builtin").find_files({
+          search_dirs = { "files" },
+          no_ignore = true,   -- Игнорировать .gitignore для этой папки
+        })
+      end,
+      desc = "Find in files/",
+    },
     { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Search by text" },
     { "<leader>fw", "<cmd>Telescope grep_string<CR>", desc = "Search current word" },
   },
