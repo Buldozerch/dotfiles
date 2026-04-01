d754cc217b4bcd7ae4157c9d35c5cc989a6a9251
saimondanya <backfire314@gmail.com>
2026-04-01 22:10:35 +0300

    update nvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,16 +5,11 @@ map("i", "jj", "<Esc>", { desc = "Exit insert mode" })
 
 -- сохранить файл
 map("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
-
--- сохранить и выйти
 map("n", "<leader>wq", "<cmd>wq<CR>", { desc = "Save and quit" })
-
 map("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit" })
 
--- открыть/закрыть дерево файлов
+-- файловое дерево
 map("n", "<leader>e", "<cmd>Neotree toggle<CR>", { desc = "Toggle file tree" })
-
--- показать текущий файл в дереве
 map("n", "<leader>ef", "<cmd>Neotree reveal<CR>", { desc = "Reveal file in tree" })
 
 -- навигация между окнами
@@ -23,22 +18,9 @@ map("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 map("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
 map("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
 
--- buffers
-map("n", "<leader>n", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
-map("n", "<leader>p", "<cmd>BufferLineCyclePrev<CR>", { desc = "Prev buffer" })
-map("n", "<leader>1", "<cmd>BufferLineGoToBuffer 1<CR>", { desc = "Buffer 1" })
-map("n", "<leader>2", "<cmd>BufferLineGoToBuffer 2<CR>", { desc = "Buffer 2" })
-map("n", "<leader>3", "<cmd>BufferLineGoToBuffer 3<CR>", { desc = "Buffer 3" })
-map("n", "<leader>4", "<cmd>BufferLineGoToBuffer 4<CR>", { desc = "Buffer 4" })
-map("n", "<leader>5", "<cmd>BufferLineGoToBuffer 5<CR>", { desc = "Buffer 5" })
-map("n", "<leader>6", "<cmd>BufferLineGoToBuffer 6<CR>", { desc = "Buffer 6" })
-map("n", "<leader>7", "<cmd>BufferLineGoToBuffer 7<CR>", { desc = "Buffer 7" })
-map("n", "<leader>8", "<cmd>BufferLineGoToBuffer 8<CR>", { desc = "Buffer 8" })
-map("n", "<leader>9", "<cmd>BufferLineGoToBuffer 9<CR>", { desc = "Buffer 9" })
-map("n", "<leader>0", "<cmd>BufferLineGoToBuffer 10<CR>", { desc = "Buffer 10" })
--- buffer close
-map("n", "<leader>x", "<cmd>Bdelete<CR>", { desc = "Close current buffer" })
-map("n", "<leader>X", "<cmd>Bdelete!<CR>", { desc = "Force close current buffer" })
+-- закрыть буфер
+map("n", "<leader>x", "<cmd>bd<CR>", { desc = "Close buffer" })
+map("n", "<leader>X", "<cmd>bd!<CR>", { desc = "Force close buffer" })
 
 -- diagnostics
 map("n", "<leader>dd", vim.diagnostic.open_float, { desc = "Show diagnostic" })
